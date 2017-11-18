ed2bffdc9c655b99aafb0663ae84889d1497c6f3
zare <elzarebcn@gmail.com>
2017-11-18 23:39:55 +0100

    ahora minDifference tiene un coste de 0 (n), añadida doc a los metodos

--- src/AlgorithmsExercises.java
+++ src/AlgorithmsExercises.java
@@ -1,7 +1,5 @@
 import javax.management.RuntimeErrorException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class AlgorithmsExercises {
 
@@ -14,18 +12,27 @@ public class AlgorithmsExercises {
         List<Integer> list1 = new ArrayList<>(Arrays.asList(8, 3, 12, 2, 5));
         System.out.println(minimumDifference(list1));
 
+        Person p1 = new Person("Peter", 20, "NY");
+        Person p2 = new Person("Mary", 30, "London");
+        Person p3 = new Person("Peter", 20, "BCN");
+
+        List<Person> list2 = new ArrayList<>(Arrays.asList(p1, p2, p3));
+        System.out.println(findRepeatedPerson(list2));
+
     }
 
+    // Finds the maximum number of the List by iterating it and comparing number by number
+    // a fast method is sorting the List and picking the last number of the List
     private static int findTheMaximum (List<Integer> list) {
 
         int max = 0;
 
         if (list.size() > 0) {
 
-            for (int i = 0; i < list.size(); i++) {
+            for (int num : list) {
 
-                if (max < list.get(i)) {
-                    max = list.get(i);
+                if (max < num) {
+                    max = num;
                 }
             }
         } else {
@@ -34,7 +41,11 @@ public class AlgorithmsExercises {
         return max;
     }
 
+    // Finds the 2 numbers with the minimum difference between them by sorting the List and
+    // iterating the List comparing the difference between consecutive couples of numbers
     private static String minimumDifference (List<Integer> list) {
+
+        Collections.sort(list);
 
         int num = list.get(0);
         int num1 = list.get(1);
@@ -42,17 +53,48 @@ public class AlgorithmsExercises {
 
         for (int i = 0; i < list.size() - 1; i++) {
 
+            if (Math.abs(list.get(i) - list.get(i + 1)) < minDiff) {
+
+                num = list.get(i);
+                num1 = list.get(i + 1);
+                minDiff = Math.abs(num - num1);
+            }
+
+        }
+
+        return "(" + num + ", " + num1 + ")";
+    }
+
+    static class Person {
+
+        String name;
+        int age;
+        String city;
+
+        public Person (String name, int age, String city) {
+
+            this.name = name;
+            this.age = age;
+            this.city = city;
+        }
+    }
+
+
+    private static boolean findRepeatedPerson (List<Person> list) {
+
+        for (int i = 0; i < list.size() - 1; i++) {
+
             for (int j = i + 1; j < list.size(); j++) {
 
-                if ((list.get(0) - list.get(j)) < minDiff) {
+                Person a = list.get(i);
+                Person b = list.get(j);
 
-                    num = list.get(i);
-                    num1 = list.get(j);
-                    minDiff = Math.abs(num - num1);
+                if (a.name.equals(b.name) && (a.age == b.age)) {
+                    return true;
                 }
             }
         }
-
-        return "(" + Math.min(num, num1) + ", " + Math.max(num, num1) + ")";
+        return false;
     }
+
 }
